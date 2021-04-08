1172cb81ee583295d3051e4c1d0adfb132a08328
BahadirIren <bahadiriren@gmail.com>
2021-04-08 18:17:45 +0300

    binary search recursive added

--- Search Algorithms/Binary Search/binay_search.cpp
+++ Search Algorithms/Binary Search/binay_search.cpp
@@ -3,6 +3,20 @@
     Search Algorithms
         Fill an array with N random numbers and search for an integer x value (binary search)
             1- Iterative Algorithm
+            2- Recursive Algoritm
+
+            For iterative version: 
+                space complexity is constant, 
+                time complexity is N, N/2, N/4, N/8, ..., N/2^d => 1 = N/2^d => logN = d
+
+            For recursive version:
+                space complexity ?
+                time complexity ?
+                T(N) = T(N/2) + 1
+                T(N/2) = T(N/4) + 1
+                ....
+                T(2) = T(1) + 1
+                Simplfy this set of equations so that you get T(N) = O(logN)
             
 */
 
@@ -13,6 +27,7 @@
 
 // prototype
 int binarySearch(int *a, int x, int low, int high);
+int binarySearchRec(int *a, int x, int low, int high);
 void sort(int *a, int size);
 void print(int *a, int size);
 
@@ -28,26 +43,40 @@ int main()
     print(a, N);
     sort(a, N);
     print(a, N);
-    std::cout << binarySearch(a, 5, N - 1, 0) << std::endl;
+    std::cout << binarySearch(a, 5, 0, N) << std::endl;
+    std::cout << binarySearchRec(a, 5, 0, N) << std::endl;
 }
 
 int binarySearch(int *a, int x, int low, int high)
 {
-    // because array is sorted descending
-    while (low >= high)
+
+    while (low <= high)
     {
         int mid = (low + high) / 2;
         if (a[mid] < x)
-            low = mid - 1;
+            low = mid + 1;
         else if (a[mid] > x)
-            high = mid + 1;
+            high = mid - 1;
         else
             return mid;
     }
     return NOT_FOUND;
 }
 
-// insertion sort, descending order
+int binarySearchRec(int *a, int x, int low, int high)
+{
+    int mid = (low + high) / 2;
+    if (low > high)
+        return NOT_FOUND;
+    if (a[mid] < x)
+        return binarySearchRec(a, x, mid + 1, high);
+    else if (a[mid] > x)
+        return binarySearchRec(a, x, low, mid - 1);
+    else
+        return mid;
+}
+
+// insertion sort, ascending order
 void sort(int *a, int size)
 {
     int insertValue, j;
@@ -56,7 +85,7 @@ void sort(int *a, int size)
     {
         insertValue = a[i];
         j = i;
-        while ((j > 0) && (a[j - 1] < insertValue))
+        while ((j > 0) && (a[j - 1] > insertValue))
         {
             a[j] = a[j - 1];
             j--;
